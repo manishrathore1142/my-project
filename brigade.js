27fd0d4e73686888b4dcb5211ff32957738fa68d
Manish Kumar <manish.kumar1@successive.tech>
2019-10-15 12:18:25 +0530

    second commit

--- brigade.js
+++ brigade.js
@@ -4,7 +4,8 @@ events.on("push", () => {
   var job = new Job("do-nothing", "alpine:3.4")
   job.tasks = [
     "echo Hello",
-    "echo World"
+    "echo World",
+    "echo Manish"
   ]
 
   job.run()
